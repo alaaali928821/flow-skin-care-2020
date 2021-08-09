58fece443237473839be566ae1fbb7dc4470f33a
Alaa <alaa.kortoma@vodafone.com>
2021-08-09 19:23:23 +0200

    add main

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainViewComponent } from './main-view/main-view.component';
+import { TopMenuComponent } from './main-view/top-menu/top-menu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainViewComponent,
+    TopMenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
